a17edba9ee7c98dc8a0a94b4615afd06a1664d70
Jesse Emerson <jesseemerson7@gmail.com>
2023-05-07 17:03:50 -0400

    adding handlebars views and paths in controller

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,7 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.get('/', async (req, res) => {
-  res.render('homepage');
+router.get("/", async (req, res) => {
+  res.render("homePage");
 });
 
 module.exports = router;
