cc202333304c062f8ca9a258906c264650c741d7
Jesse Emerson <jesseemerson7@gmail.com>
2023-05-08 23:53:55 -0400

    added sign up post and data is being sent to database

--- controllers/signUpRoutes.js
+++ controllers/signUpRoutes.js
@@ -1,22 +1,26 @@
-const router = require('express').Router()
-const  { User }  = require('../models')
+const router = require("express").Router();
+const { User } = require("../models");
 
+router.get("/", async (req, res) => {
+  res.render("signUp");
+});
 
-
-router.get("/", async (req, res)=>{
-    res.render('signUp');
-})
-
-router.post("/", async (req, res)=>{
+router.post("/", async (req, res) => {
   try {
-   
-    
+    const newUsername = req.body.username;
+    const newUserPassword = req.body.password;
+    const newUser = await User.create({
+      username: newUsername,
+      password: newUserPassword,
+    });
+    if (!newUser) {
+      res.status(400).json();
+    } else {
+      res.status(200).json();
+    }
   } catch (error) {
     res.status(400).json(error);
   }
-  
- 
-})
+});
 
-
-module.exports = router
+module.exports = router;
