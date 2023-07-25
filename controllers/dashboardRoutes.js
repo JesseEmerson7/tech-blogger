cb6245e8a0cbb01ae46e9e233871591f6e8bab0b
Jesse Emerson <jesseemerson7@gmail.com>
2023-07-25 00:35:41 -0400

    added data to dash board page render

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,25 +1,25 @@
 const router = require("express").Router();
+const { where } = require("sequelize");
 const { User, Post, Comment } = require("../models/index");
+const { raw } = require("express");
 
 router.get("/", async (req, res) => {
   if (!req.session.loggedIn) {
     console.log("not logged in");
     res.redirect("login");
   } else {
-    const userData = await Post.findAll({
-      where: { UserId: req.session.UserId },
-      include: [
-        {
-          model: User,
-          attributes: ["username"],
-        },
-      ],
-      raw: true,
-    });
-    console.log(userData);
+    const userData = await User.findOne(
+      {
+        where:{id: req.session.UserId}
+      },
+      {raw: true}
+    );
+
+    let {username } = userData.dataValues;
+    console.log(userData.dataValues);
     console.log("logged in");
     console.log(req.session.UserId);
-    res.render("dashboard", { userData });
+    res.render("dashboard", { username });
   }
 });
 
