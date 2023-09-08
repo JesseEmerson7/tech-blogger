7d35b115df7e039a16110a76b1ceb95ca2a5e4a8
Jesse Emerson <jesseemerson7@gmail.com>
2023-09-07 22:31:55 -0400

    adding delete function

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -10,18 +10,38 @@ router.get("/", async (req, res) => {
   } else {
     const userData = await User.findOne(
       {
-        where:{id: req.session.UserId},
-        include: Post
+        where: { id: req.session.UserId },
+        include: Post,
       },
-      
-      {raw: true}
+
+      { raw: true }
     );
 
-    let {username, Posts } = userData.dataValues;
+    let { username, Posts } = userData.dataValues;
     console.log(userData.dataValues);
     console.log("logged in");
     console.log(req.session.UserId);
     res.render("dashboard", { username, Posts });
+  }
+});
+
+//deleting post from delete button
+
+router.post("/delete", async (req, res) => {
+  if (!req.session.loggedIn) {
+    console.log("not logged in");
+    res.redirect("login");
+  } else {
+    console.log(req.body);
+    const postDelete = await Post.destroy(
+      {
+        where: { id: req.body.id },
+      },
+
+      { raw: true }
+    );
+
+    console.log(postDelete);
   }
 });
 
--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,5 +1,5 @@
-
 const createBtn = document.getElementById("create-post");
+const feed = document.getElementById("userFeed");
 
 const handleCreateBtn = () => {
   console.log("clicking");
@@ -7,5 +7,21 @@ const handleCreateBtn = () => {
 };
 
 //todo: edit and delete button for each
+const handleDeleteBtn = async (e) => {
+  if (e.target.className == "deleteBtn") {
+    let data = e.target.getAttribute("data");
+    console.log(data);
+    try {
+      const postData = await fetch("/dashboardRoutes/delete", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ data: data }),
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+};
 
 createBtn.addEventListener("click", handleCreateBtn);
+feed.addEventListener("click", handleDeleteBtn);
